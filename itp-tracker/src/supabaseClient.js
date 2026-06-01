ebd53b0f9b6a5c989ee398f7be35699091784435
Vlad <vladcornos@yahoo.com>
2026-06-01 13:18:35 +0300

    fix key

--- itp-tracker/src/supabaseClient.js
+++ itp-tracker/src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://shuqoxdyblinxuwpasot.supabase.co'
-const supabaseKey = 'sb_publishable_GaghEvL0sU2AuH2EZ4V4Wg_zAPFBLBM'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNodXFveGR5Ymxpbnh1d3Bhc290Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAyODYyNDgsImV4cCI6MjA5NTg2MjI0OH0.OfJoOGTIc7Zh7u0QX7Y-9X3gUB5vBQKtJIdlHJONlRY'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
